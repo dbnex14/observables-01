12ab3edf874f94e2fc8b2db8e3f5902c1f4465f0
Buljubasic <KI0V@icbc.net>
2020-06-08 12:35:12 -0700

    Added comment explaining observable.  Next we will build our won observable in order to understand them better.

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -13,6 +13,11 @@ export class UserComponent implements OnInit {
   }
 
   ngOnInit() {
+    // since this is observable, clicking on user 1 or 2 will update this id bound in template.
+    // We subscribe here to params observable which is stream of data we subscribe to listen to.
+    // We then subscribe to it and provide a function or callback to execute our code to receive
+    // the data change.
+    // That is all fine but in order to better understand observables, let's build our own.
     this.route.params.subscribe((params: Params) => {
       this.id = +params.id;
     });
